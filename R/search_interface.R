c82ceba0c5f3a1667bcb1fc951ce57529d292340
SimonLyons <simon_lyons@hotmail.com>
2017-03-31 16:56:03 +1000

    End of day commit. Progress update in search_interface.R

--- R/search_interface.R
+++ R/search_interface.R
@@ -16,22 +16,35 @@
 # resources, tags, groups, organization, name, url, notes, owner_org,
 # data_state, title, contact_info
 
-Y <- function(){
+ozdata_usr_srch <- function(){
+    require(ckanr)
+
+    # Set up ckanr link to data.gov.au
+    ckanr_setup(url="http://data.gov.au/")
+
     # Perform check for existing oz_metadata file
     # [insert code for this check]
 
+    # Perform update check. Does existing metadata file need updating
+    # Look at date modfied of local file
+    # Pull out most recent change to ckanr data.
+    changes(limit = 2, as = "table")[, 1:4]
+
+
+    # create temporary files and directories
+    tmp_file <- tempfile()
+    tmp_dir <- file.path(tempdir(), basename(tempfile()))
+    dir.create(tmp_dir, showWarnings = FALSE, recursive = TRUE)
+
+    if(oz_meta_check = NULL){download_oz_metadata()}
     # Ask user if they want to download the lastest metadata (returns integere 1 for Yes or 2 for No)
     dl_meta <- menu(c("Yes", "No"), title = "Do you want to download the latest data.gov.au metadata?")
-
-    if(dl_meta == 1, download_oz_metadata)
+    # Download
+    if(dl_meta == 1) {download_oz_metadata()}
     search_term <- readline(prompt = "Enter your keyword search term: ")
     ?readline()
-library(shiny)
 
-# response <- utils::menu(c("Yes", "No"), title="Do you want this?", graphics = TRUE)
 
-# Input keyword
-?menu
 
 }  # end 'ozdata_user_input
 
