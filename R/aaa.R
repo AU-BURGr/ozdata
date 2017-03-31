ee404610d54276e070f2c61c66de7636e11a28a9
Jonathan Carroll <jono@jcarroll.com.au>
2017-03-31 13:02:32 +1030

    package documentation

--- R/aaa.R
+++ R/aaa.R
@@ -1,5 +1,22 @@
-#' ozdata
+#' ozdata: Collect Australian data sets and work with them
 #'
+#' Obtain data from data.gov.au and ozmacrodata.org, process it, plot it.
+#'
+#' Created as part of the ozdata17 unconf in Brisbane, Australia.
+#'
+#' Authors:
+#'
+#' * [Jonathan Carroll](https://github.com/jonocarroll)
+#' * [Maria Dmitrijeva](https://github.com/marianess)
+#' * [Jeff Hanson](https://github.com/jeffreyhanson)
+#' * [Rob Hyndman](https://github.com/robjhyndman)
+#' * [Simon Lyons](https://github.com/SimonLyons)
+#' * [Cameron Roach](https://github.com/camroach87)
+#' * [Matt Sutton](https://github.com/matt-sutton)
+#'
+#' For a general overview of the steps taken to produce this, see `vignette("ozdata")`.
+#'
+#' @md
 #' @name ozdata
 #' @docType package
 NULL
