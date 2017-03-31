2dc7ec33a5476e3ca1f2f9908e48e98916b6d676
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2017-03-31 12:19:48 +1000

    rename datasets to oz_datasets

--- R/oz_datasets.R
+++ R/oz_datasets.R
@@ -1,18 +1,18 @@
 #' @include internal.R
 NULL
 
-#' Datasets
+#' Australian datasets
 #'
 #' List all available datasets.
 #'
 #' @return \code{link[tibble]{tibble}}
 #'
 #' @export
-datasets <- function() {
+oz_datasets <- function() {
   # set up api access
   ckanr::ckanr_setup(url = "data.gov.au")
   # download ids
   ids <- ckanr::package_list(as = "table", limit = NULL)
   # download metatadata and convert to data
-  do.call(rbind, lapply(ids, ckanr::package_show))
+  do.call(rbind, lapply(ids, ckanr::package_show, as = "table"))
 }
