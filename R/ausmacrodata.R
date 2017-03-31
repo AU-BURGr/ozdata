56cc346ab93040753418960ffe7d7f5e91cd7b4b
Rob J Hyndman <robjhyndman@gmail.com>
2017-03-31 17:54:01 +1100

    Added xts functionality.

--- R/ausmacrodata.R
+++ R/ausmacrodata.R
@@ -1,78 +1,91 @@
 #' Read data from ausmacrodata.org
 #'
-#' \code{ausmacro} returns a data set from \url{ausmacrodata.org}.
-#' By default, it will return a time series (a `ts` object), although
-#' a `tibble` is also possible (and probably preferable for daily data).
+#' \code{get_ausmacrodata} returns a time series data set from \url{ausmacrodata.org}.
 #'
 #' @param dataset a URL for a specific data set on ausmacrodata.org, or an id for a data set.
-#' @param format The desired format for the object to be returned.
+#' @param format The desired format for the object to be returned. Possible
+#' values are \code{ts}, \code{xts} and \code{tibble}. By default a \code{ts}
+#' object is returned for quarterly, monthly or annual data, and a
+#' \code{xts} object is returned for daily data.
 #' @param ... Other arguments, not currently used.
 #'
 #' @author Rob J Hyndman
 #'
 #' @export
 #' @examples
-#' y <- ausmacro("http://ausmacrodata.org/series.php?id=gdpcknaasaq")
+#' y <- get_ausmacrodata("http://ausmacrodata.org/series.php?id=gdpcknaasaq")
 #' plot(y)
-#' z <- ausmacro("gdpcknaasaq")
+#' library(xts)
+#' z <- get_ausmacrodata('crtdoirymmdir')
+#' plot(z)
 
-ausmacro <- function(dataset, format=c("ts","tibble"), ...)
+get_ausmacrodata <- function(dataset, format, ...)
 {
-    format <- match.arg(format)
+  # Find last occurrence of "=" in dataset
+  id <- utils::tail(strsplit(dataset, "=")[[1]],1)
 
-    # Find last occurrence of "=" in dataset
-    id <- utils::tail(strsplit(dataset, "=")[[1]],1)
+  # Create URL
+  url <- paste("http://ausmacrodata.org/series.php?id=", id, sep="")
 
-    # Create URL
-    url <- paste("http://ausmacrodata.org/series.php?id=", id, sep="")
+  # Scrape page to find csv file
+  links <- rvest::html_attr(rvest::html_nodes(xml2::read_html(url), "a"), "href")
+  datalink <- stringr::str_subset(links, "\\.csv")
 
-    # Scrape page to find csv file
-    links <- rvest::html_attr(rvest::html_nodes(xml2::read_html(url), "a"), "href")
-    datalink <- stringr::str_subset(links, "\\.csv")
+  # Check there is only one
+  if(length(datalink) > 1)
+  {
+    warning("More than one csv file found. Using first one")
+    datalink <- datalink[1]
+  }
 
-    # Check there is only one
-    if(length(datalink) > 1)
+  # Read data
+  dataurl <- paste("http://ausmacrodata.org", datalink, sep="")
+  y <- readr::read_csv(dataurl)
+  dates <- y[["date"]]
+  splitdates <- stringr::str_split(dates,"/")
+  dailydata <- (nchar(dates[1]) == 10L)
+
+  # Set format
+  if(missing(format))
+    format <- ifelse(dailydata, "xts", "ts")
+
+  # Set up data in required format
+  if(format=="tibble")
+  {
+    return(y[,c("date","value")])
+  }
+  else if(format=="xts")
+  {
+    if(nchar(dates[1])==7L)
+      dates <- paste("01/",dates,sep="")
+    y <- xts::as.xts(y[['value']],
+            order.by=as.Date(dates, format="%d/%m/%Y"))
+    return(y)
+  }
+  else if(format=="ts")
+  {
+    if(dailydata)
     {
-        warning("More than one csv file found. Using first one")
-        datalink <- datalink[1]
+      years <-  as.numeric(unlist(lapply(splitdates, function(x)x[[3]])))
+      tblyears <- table(years)
+      daysperyear <- max(tblyears)
+      y <- stats::ts(y[['value']],
+          frequency=daysperyear,
+          start=c(years[1]-1 + tblyears[1]/daysperyear))
     }
-
-    # Read data
-    dataurl <- paste("http://ausmacrodata.org", datalink, sep="")
-    y <- readr::read_csv(dataurl)
-
-    if(format=="tibble")
+    else
     {
-        return(y[,c("date","value")])
+      #We have monthly, quarterly or annual data
+      seasons <- as.numeric(unlist(lapply(splitdates, function(x)x[[1]])))
+      years <-  as.numeric(unlist(lapply(splitdates, function(x)x[[2]])))
+      useasons <- sort(unique(as.numeric(seasons)))
+      y <- stats::ts(y[['value']],
+        frequency=length(useasons),
+        start=c(years[1], match(seasons[1], useasons)))
     }
-    else #return a ts
-    {
-        # Now check time attributes
-        dates <- y[["date"]]
-        dates <- stringr::str_split(dates,"/")
-        if(length(dates[[1]]) == 3)
-        {
-            # We have daily data
-            years <-  as.numeric(unlist(lapply(dates, function(x)x[[3]])))
-            tblyears <- table(years)
-            daysperyear <- max(tblyears)
-            y <- stats::ts(y[['value']],
-                    frequency=daysperyear,
-                    start=c(years[1]-1 + tblyears[1]/daysperyear))
-        }
-        else if(length(dates[[1]])==2)
-        {
-            #We have monthly, quarterly or annual data
-            seasons <- as.numeric(unlist(lapply(dates, function(x)x[[1]])))
-            years <-  as.numeric(unlist(lapply(dates, function(x)x[[2]])))
-            useasons <- sort(unique(as.numeric(seasons)))
-            y <- stats::ts(y[['value']],
-                frequency=length(useasons),
-                start=c(years[1], match(seasons[1], useasons)))
-        }
-        else
-            stop("Not sure what to do now")
-        return(y)
-    }
+    return(y)
+  }
+  else
+    stop("I can't understand this format")
 }
 
