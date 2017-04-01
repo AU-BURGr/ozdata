ea6cbbea93d525aaa8086beacb493fe5bde1885f
SimonLyons <simon_lyons@hotmail.com>
2017-04-01 10:49:41 +1000

    Updates to Shiny search interface.

--- shinySearch/server.R
+++ shinySearch/server.R
@@ -3,10 +3,14 @@ require(shiny)
 shinyServer(function(input, output) {
 
     # You can access the value of the widget with input$text, e.g.
-    output$value <- renderPrint({ input$text })
+    output$value1 <- renderPrint({ input$text })
 
     output$text1 <- renderText({
         paste("You have selected", input$select)
+    })
+
+    output$text2 <- renderText({
+        paste("Your date range is ", input$dates[[1]], " to ", input$dates[[2]])
     })
 
 
--- shinySearch/ui.R
+++ shinySearch/ui.R
@@ -6,40 +6,41 @@ myUI <- shinyUI(fluidPage(
 
     sidebarLayout(
         # Sidebar panel which contains the input options for user selection
+        # Define sidebar panel width
         sidebarPanel(
             helpText("Welcome to the R Project interface for searching Australian government open data."),
-            br(),
 
         # text search input box
         textInput("text", label = h5("Input search keyword"), value = "Enter text..."),
 
-        hr(),
-        fluidRow(column(10, verbatimTextOutput("value"))),
-
         # Insert date range selection option
         fluidRow(
-            column(10,
+            column(12,
                    dateRangeInput("dates", label = h5("Date range")))),
 
         # Insert drop down box with data.gov.au catetegory selections
-        column(10,
+        column(12,
                selectInput("select", label = h5("data.gov.au groups"),
                            choices = list("No selection" = 1, "Civic" = 2,
                                           "Cultural" = 3, "Defence" = 4,
                                           "Environment" = 5, "Finance" = 6,
                                           "Health" = 7, "Immigration" = 8), selected = 1)),
 
-        br(),
-        img(src = "highres_456807711.png", height = 36, width = 36),
-        "BURGr UnConf 2017",
-        br(),
+        # There has to be a better way for formatting this break....
+        br(), br(), br(), br(), br(),
+
+        # Insert BUGr image and licence conditions
+        img(src="highres_456807711.png", height = 40, width = 40), "BURGr UnConf 2017",
         p("Insert licence conditions - e.g. GPL2", style = "font-family: 'times'; font-size:7pt")
         ),   # end 'sidebarPanel'
 
+        # Define shiny Main Panel
         mainPanel(h4("Main Panel - yet to be defined", align = "center"),
                   p("This area will be used to display search output tables"),
 
-                  textOutput("text1")   # Reactive output from server.R file
+                  testOutput("value1"),
+                  textOutput("text1"),
+                  textOutput("text2")   # Reactive output from server.R file
                   )   # end 'mainPanel'
 
     )   # end 'sidebarLayout'
