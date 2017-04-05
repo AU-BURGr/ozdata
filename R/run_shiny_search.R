bf6da0e0e993bad0ca0bb1a5f208e2d5565e95dd
Cameron Roach <camroach87@gmail.com>
2017-04-05 11:48:33 +1000

    Added shiny app documentation.

--- R/run_shiny_search.R
+++ R/run_shiny_search.R
@@ -1,3 +1,14 @@
+#' Run shiny search app
+#'
+#' This function runs the shiny search app for data.gov.au.
+#'
+#' @return A shiny application
+#'
+#' @author Simon Lyons, Cameron Roach
+#'
+#' @export
+#'
+#' @examples
 run_shiny_search <- function()
 {
     shiny::runApp(appDir = system.file("shiny_search", package = "ozdata"))
